7a8645f8af40179fac7f45ea7b68a16bc645f7f2
ertegin <calikersn2001@gmail.com>
2026-09-06 18:50:23 +0300

    env added

--- src/db/firebase.jsx
+++ src/db/firebase.jsx
@@ -1,22 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase} from "firebase/database";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCMP3ul02J23uyAKsoQi1X1Y3fkI011hts",
-  authDomain: "chatapp-a6315.firebaseapp.com",
-  databaseURL: "https://chatapp-a6315-default-rtdb.firebaseio.com",
-  projectId: "chatapp-a6315",
-  storageBucket: "chatapp-a6315.firebasestorage.app",
-  messagingSenderId: "601763680576",
-  appId: "1:601763680576:web:0449ba0bf04f47baf8eaa5",
-  measurementId: "G-369M6B7P9J"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+export const auth = getAuth(app);
